1312f4c413d6601673a932cad64607a5ab3c378b
davidaskenazy <davidask.da@gmail.com>
2021-01-09 22:39:51 -0300

    model entitys finished

--- src/main/java/com/david/reddit/model/VoteType.java
+++ src/main/java/com/david/reddit/model/VoteType.java
@@ -1,4 +1,26 @@
 package com.david.reddit.model;
 
-public class VoteType {
+
+
+import java.util.Arrays;
+
+public enum VoteType {
+    UPVOTE(1), DOWNVOTE(-1),
+    ;
+
+    private int direction;
+
+    VoteType(int direction) {
+    }
+
+    public static VoteType lookup(Integer direction) {
+        return Arrays.stream(VoteType.values())
+                .filter(value -> value.getDirection().equals(direction))
+                .findAny()
+                .orElseThrow(() -> new Exception());
+    }
+
+    public Integer getDirection() {
+        return direction;
+    }
 }
